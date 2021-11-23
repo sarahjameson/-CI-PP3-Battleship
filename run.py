0d95d6f7754b7ccf6371bfe0121096e3e06fd6a4
Sarah Jameson <meowells@gmail.com>
2021-11-23 16:15:42 +0000

    Show battleships

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 class GameBoard(object):
 
     def __init__(self, battleships, board_width, board_height):
@@ -12,15 +13,21 @@ class GameBoard(object):
     # update battleship with hits
     # save if hit or miss
     def take_shot(self, shot_location):
+        hit_battleship = None
         is_hit = False
         for b in self.battleships:
             idx = b.body_index(shot_location)
             if idx is not None:
                 is_hit = True
                 b.hits[idx] = True
+                hit_battleship = b
                 break
 
         self.shots.append((Shot(shot_location, is_hit)))
+        return hit_battleship
+
+    # game over function
+
 
 class Shot(object):
 
@@ -46,62 +53,78 @@ class Battleship(object):
 
             body.append(el)
 
-        return Battleship(body)
+        return Battleship(body, direction)
 
-    def __init__(self, body):
+
+    def __init__(self, body, direction):
         self.body = body
+        self.direction = direction
         self.hits = [False] * len(body)
+
 
     def body_index(self, location):
         try:
-            return b.body.index(location)
+            return self.body.index(location)
         except ValueError:
             return None
 
 
-def render(board_width, board_height, shots):
-    header = "+" + "-" * board_width + "+"
+    # destroyed function
+
+
+# player class
+
+
+def render(game_board, show_battleships=False):
+    header = "+" + "-" * game_board.board_width + "+"
     print(header)
 
-    shots = set(shots)
-    for y in range(board_height):
-        row = []
-        for x in range(board_width):
-            if (x, y) in shots:
-                ch = "X"
-            else:
-                ch = " "
-            row.append(ch)
-        print("|" + "".join(row) + "|")
-        print("|" + " " * board_width + "|")
-
-    print(header)
-
-
-def render_battleships(board_width, board_height, battleships):
-    header = "+" + "-" * board_width + "+"
-    print(header)
-
-    # make board
+    # Construct empty board
     board = []
-    for x in range(board_width):
-        row = []
-        for y in range(board_height):
-            row.append(None)
-        board.append(row)
+    for _ in range(game_board.board_width):
+        board.append([None for _ in range(game_board.board_height)])
 
-    # add battleships
-    for b in battleships:
-        for x, y in b.body():
-            board[x][y] = "O"
+    if show_battleships:
+        # Add the battleships to the board
+        for b in game_board.battleships:
+            for i, (x, y) in enumerate(b.body):
+                if b.direction == "N":
+                    chs = ("v", "|", "^")
+                elif b.direction == "S":
+                    chs = ("^", "|", "v")
+                elif b.direction == "W":
+                    chs = (">", "=", "<")
+                elif b.direction == "E":
+                    chs = ("<", "=", ">")
+                else:
+                    raise "Unknown direction"
 
-    for y in range(board_height):
+                if i == 0:
+                    ch = chs[0]
+                elif i == len(b.body) - 1:
+                    ch = chs[2]
+                else:
+                    ch = chs[1]
+                board[x][y] = ch
+
+    # Add the shots to the board
+    for sh in game_board.shots:
+        x, y = sh.location
+        if sh.is_hit:
+            ch = "X"
+        else:
+            ch = "@"
+        board[x][y] = ch
+
+    for y in range(game_board.board_height):
         row = []
-        for x in range(board_width):
-            row.append((x, y))
-        print("".join(row))
-    
+        for x in range(game_board.board_width):
+            row.append(board[x][y] or " ")
+        print("|" + "".join(row) + "|")
+
     print(header)
+
+
 
 
 if __name__ == "__main__":
@@ -111,13 +134,12 @@ if __name__ == "__main__":
         Battleship.build((2, 3), 4, "E")
     ]
 
-    for b in battleships:
-        print(b.body)
-
     game_board = GameBoard(battleships, 10, 10)
     shots = [(1, 1), (0, 0), (5, 7)]
     for sh in shots:
         game_board.take_shot(sh)
+
+    render(game_board)
 
     for sh in game_board.shots:
         print(sh.location)
@@ -127,8 +149,8 @@ if __name__ == "__main__":
         print(b.body)
         print(b.hits)
         print("==========")
-    print(game_board.shots)
-    print(game_board.battleships)
+   
+    render(10, 10, game_board.shots)
 
     exit(0)
 
