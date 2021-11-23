9586f2ba9cce1043c070083d4f8766ddc3e8428b
Sarah Jameson <meowells@gmail.com>
2021-11-23 13:27:58 +0000

    Create battleship class

--- run.py
+++ run.py
@@ -1,6 +1,28 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Battleship(object):
+
+    @staticmethod
+    def build(head, length, direction):
+        body = []
+        for i in range(length):
+            if direction == "N":
+                el = (head[0], head[1] - i)
+            elif direction == "S":
+                el = (head[0], head[1] + i)
+            elif direction == "W":
+                el = (head[0] - i, head[1])
+            elif direction == "E":
+                el = (head[0] + i, head[1])
+
+            body.append(el)
+
+        return Battleship(body)
+
+    def __init__(self, body):
+        self.body = body
+
 
 def render(board_width, board_height, shots):
     header = "+" + "-" * board_width + "+"
