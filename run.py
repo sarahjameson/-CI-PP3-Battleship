ca167b16dc71958724107add2cc5c3e5355700ca
Sarah Jameson <meowells@gmail.com>
2021-11-23 17:14:27 +0000

    Try to get turns working

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import copy
 
 class GameBoard(object):
 
@@ -56,12 +57,10 @@ class Battleship(object):
 
         return Battleship(body, direction)
 
-
     def __init__(self, body, direction):
         self.body = body
         self.direction = direction
         self.hits = [False] * len(body)
-
 
     def body_index(self, location):
         try:
@@ -69,12 +68,15 @@ class Battleship(object):
         except ValueError:
             return None
 
-
     def is_destroyed(self):
         return all(self.hits)
 
 
-# player class
+class Player(object):
+
+    def __init__(self, name, shot_f):
+        self.name = name
+        self.shot_f = shot_f
 
 
 def render(game_board, show_battleships=False):
@@ -126,26 +128,104 @@ def render(game_board, show_battleships=False):
 
     print(header)
 
+# type, metadata (player,...)
+def announce_en(event_type, metadata={}):
+    if event_type == "game_over":
+        print("%s WINS THE GAME!" % metadata['player'])
+    elif event_type == "new_turn":
+        print("%s YOUR TURN!" % metadata['player'])
+    elif event_type == "miss":
+        print("%s MISSED!" % metadata['player'])
+    elif event_type == "battleship_destroyed":
+        print("%s DESTROYED a battleship!" % metadata['player'])
+    elif event_type == "battleship_hit":
+        print("%s HIT a battleship!" % metadata['player'])
+    else:
+        print("UNKNOWN EVENT TYPE: %s" % event_type)
+
+
+def announce_none(event_type, metadata={}):
+    pass
+
+
+def get_random_ai_shot(game_board):
+    x = random.randint(0, game_board.width - 1)
+    y = random.randint(0, game_board.height - 1)
+    return (x, y)
+
+
+def random_sleepy_ai(sleep_time):
+    return sleepy_ai(get_random_ai_shot, sleep_time)
+
+
+def sleepy_ai(ai_f, sleep_time):
+    def f(game_board):
+        time.sleep(sleep_time)
+        return ai_f(game_board)
+    return f
+
+
+def get_clever_ai(game_board):
+    return (0, 0)
+
+
+def get_human_shot(game_board):
+    inp = input("Where do you want to shoot?\n")
+    # TODO: deal with invalid input
+    xstr, ystr = inp.split(",")
+    x = int(xstr)
+    y = int(ystr)
+
+    return (x, y)
 
 
 
 if __name__ == "__main__":
     battleships = [
-        Battleship.build((1, 1), 2, "N"),
-        Battleship.build((5, 8), 5, "N"),
-        Battleship.build((2, 3), 4, "E")
+        Battleship.build((1,1), 2, "N"),
+        Battleship.build((5,8), 5, "N"),
+        Battleship.build((2,3), 4, "E"),
+        Battleship.build((6,6), 3, "S"),
+        Battleship.build((9,9), 5, "W"),
     ]
 
-    game_board = GameBoard(battleships, 10, 10)
+    # https://robertheaton.com/2014/02/09/pythons-pass-by-object-reference-as-explained-by-philip-k-dick/
+    game_boards = [
+        GameBoard(battleships, 10, 10),
+        GameBoard(copy.deepcopy(battleships), 10, 10)
+    ]
+
+    players = [
+        Player("Rob", random_sleepy_ai(2.5)),
+        Player("Alice", random_sleepy_ai(2.5)),
+    ]
+
+    offensive_idx = 0
     while True:
-        inp = input("Where do you want to shoot?\n")
-        x, y = inp.split(",")
-        x = int(x)
-        y = int(y)
+        # defensive player is the non-offensive one
+        defensive_idx = (offensive_idx + 1) % 2
 
-        game_board.take_shot((x, y))
-        render(game_board)
+        defensive_board = game_boards[defensive_idx]
+        offensive_player = players[offensive_idx]
 
-        if game_board.is_game_over():
-            print("You win")
+        announce_f("new_turn", {"player": offensive_player.name})
+        shot_location = offensive_player.shot_f(defensive_board)
+
+        hit_battleship = defensive_board.take_shot(shot_location)
+        if hit_battleship is None:
+            announce_f("miss", {"player": offensive_player.name})
+        else:
+            if hit_battleship.is_destroyed():
+                announce_f("battleship_destroyed", {"player": offensive_player.name})
+            else:
+                announce_f("battleship_hit", {"player": offensive_player.name})
+
+        render_f(defensive_board)
+
+        if defensive_board.is_game_over():
+            announce_f("game_over", {"player": offensive_player.name})
             break
+
+        # offensive player becomes the previous defensive
+        # player
+        offensive_idx = defensive_idx
