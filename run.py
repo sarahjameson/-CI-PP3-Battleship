caed49855b15bc8c597c90a773bcc7dfdb12b293
Sarah Jameson <meowells@gmail.com>
2021-11-23 13:05:56 +0000

    Gets input from user and renders board

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def render(board_width, board_height, shots):
+    header = "+" + "-" * board_width + "+"
+    print(header)
+
+    shots = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x, y) in shots:
+                ch = "X"
+            else:
+                ch = " "
+            row.append(ch)
+        print("|" + "".join(row) + "|")
+        print("|" + " " * board_width + "|")
+
+    print(header)
+
+
+if __name__ == "__main__":
+    shots = []
+    while True:
+        inp = input("Where do you want to shoot?\n")
+        x, y = inp.split(",")
+        x = int(x)
+        y = int(y)
+        shots.append((x, y))
+        render(10, 10, shots)
